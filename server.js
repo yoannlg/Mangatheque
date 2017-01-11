8c65dd9c37b12c6d3f9da15b1fecc7cd0d799d2e
Yoann <shinji@MacBook-Pro-de-Yoann.local>
2017-01-11 11:21:17 +0100

    ajout de diver fichiers

---
+++ server.js
@@ -0,0 +1,8 @@
+// CALL THE PACKAGES --------------------
+var express    = require('express');		// call express
+var app        = express(); 				// define our app using express
+var bodyParser = require('body-parser'); 	// get body-parser
+var morgan     = require('morgan'); 		// used to see requests
+var mongoose   = require('mongoose');
+var config 	   = require('./config');
+var path 	   = require('path');
