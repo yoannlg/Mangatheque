14e8b460d5f8eb0edb664d00e317fbbc0879d327
Yoann <shinji@MacBook-Pro-de-Yoann.local>
2017-04-06 15:43:44 +0200

    add serie schema, post and get method on series for one user

---
+++ app/models/series.js
@@ -0,0 +1,32 @@
+//utilisation de mongoose pour la création de shémas
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+
+//serie schema
+var SerieSchema = new Schema({
+	//_creator : {type: Schema.ObjectId, ref: 'User'},
+	_userId : {type: Schema.ObjectId, ref: 'User', require: true},
+	//img   : {type: },
+	titre   : {type:String, required: true},
+	nbrTotal: {type: Number, required: true},
+	//nbrAqui : {type: Number},
+	auteur  : {type: String}
+});
+
+//verify if serie is already saved
+SerieSchema.pre('save', function(next){
+	var serie = this;
+
+	//enregistre le manga seulement si le titre n'est pas déjà selectionné
+	if (serie.isSelected('titre')) return next();//res.json("ça marche pô, la série existe deja trou d'balle!");
+});
+
+// return the model
+module.exports = mongoose.model('Serie', SerieSchema);
+
+
+
+
+
+
+
