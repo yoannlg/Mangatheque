308c2401faa9d176a49381e48c06835dd0589cab
jeremie <pro_rish@yahoo.com>
2017-06-27 15:04:06 +0200

    fix schema format

--- app/models/series.js
+++ app/models/series.js
@@ -4,13 +4,19 @@ var Schema   = mongoose.Schema;
 
 //serie schema
 var SerieSchema = new Schema({
-	//_creator : {type: Schema.ObjectId, ref: 'User'},
-	_userId : {type: Schema.ObjectId, ref: 'User', require: true},
-	//img   : {type: },
-	titre   : {type:String, required: true},
-	nbrTotal: {type: Number, required: true},
-	//nbrAqui : {type: Number},
-	auteur  : {type: String}
+	// //_creator : {type: Schema.ObjectId, ref: 'User'},
+	// _userId : {type: Schema.ObjectId, ref: 'User', require: true},
+	// //img   : {type: },
+	// titre   : {type:String, required: true},
+	// nbrTotal: {type: Number, required: true},
+	// //nbrAqui : {type: Number},
+	// auteur  : {type: String}
+	name: {type: String},
+	author: {type: String},
+	category: {type: Object},
+	img: {type: String},
+	id: {type: String},
+	chapter_Length: {type: Number}
 });
 
 //verify if serie is already saved
--- app/models/user.js
+++ app/models/user.js
@@ -5,8 +5,9 @@ var bcrypt = require('bcrypt-nodejs');
 
 //user schema
 var UserSchema = new Schema({
-	name    : {type:String, required: true, index: {unique: true}},
-	password: {type: String, required: true, select: false}
+	name    : {type: String, required: true, index: {unique: true}},
+	password: {type: String, required: true, select: false},
+	manga_id: {type: Array}
 	//test : {type: schema.ObjectId, ref:'Serie'}
 });
 
