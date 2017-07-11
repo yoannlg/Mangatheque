616d0f0c54e139556ea8c7c022efa3209fd27ce1
jeremie <pro_rish@yahoo.com>
2017-07-11 17:43:22 +0200

    fix

--- public/dist/all.js
+++ public/dist/all.js
@@ -236,3 +236,13 @@ angular.module('app.routes', ['ngRoute'])
 
 //angular.module('app.routes', ['ngRoute' 'restangular'])
 
+
+window.addEventListener('click', function(e){
+	if (e.target.className === "fa fa-caret-down fa-2x pointer") {
+		console.log('oui');
+		var button = document.getElementById('order');
+		console.log(button);
+		console.log(e);
+		button.classList.toggle('rotate');
+	}
+});
